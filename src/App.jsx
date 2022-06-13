340e0fe5b6bcdce5d9e5ec4a30492f537e46192a
vinicius <vmm_7@hotmail.com>
2022-06-13 19:54:36 -0300

    início do form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
+import FormInput from "./components/FormInput";
 
 function App() {
-  return <div className="App">
-    <form>
-
-      
-    </form>
-  </div>;
+  return (
+    <div className="App">
+      <form>
+        <FormInput placeholder="Username" />
+        <FormInput placeholder="Email"/>
+        <FormInput placeholder="Full Name"/>
+        <FormInput placeholder="Else"/>
+      </form>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/FormInput.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./formInput.css";
+
+function FormInput(props) {
+  return (
+    <div className="formInput">
+      {/* <label htmlFor="">Username</label> */}
+      <input placeholder={props.placeholder}/>
+    </div>
+  );
+}
+
+export default FormInput;
